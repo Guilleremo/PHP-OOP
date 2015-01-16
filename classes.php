4eb881a968c4f523a979b6411c79565ab28e23b4
Guillermo Andrew Hermosillo <g.andrewhermosillo@gmail.com>
2015-01-15 22:50:46 -0800

    fixed all of my errors

--- classes.php
+++ classes.php
@@ -1,116 +1,110 @@
 <?php
 	class Cat {
 		public $firstName;
-		public $lastName;
-		public $gender "male";
-		public $price = "0";
+		public $lastName;
+		public $gender = "male";
+		public $price = "0";
 
 		function getName() {
 		return "{$this->firstName}" .
-		"{$this->lastName}";
+		"{$this->lastName}";
 	}
 }
-		$cat1 = new Cat();
-		$cat1->firstName = "Juan";
-		$cat1->lastName = "Carlos";
-		print "The cat’s name is {$cat1>getName()}.";
 
 	class Tiger {
-		public $firstName = "";
-		public $lastName = "";
-		public $gender = "female";
-		public $price = "$100,000";
+		public $firstName = "";
+		public $lastName = "";
+		public $gender = "female";
+		public $price = "$100,000";
 
 		function getName() {
 		return "{$this->firstName}" .
-		"{$this->lastName}";
+		"{$this->lastName}";
 	}
 }
-		$tiger1 = new Tiger();
-		$tiger1->firstName = "Juanita";
-		$tiger1->lastName = "Lopez";
-		print "The cat’s name is {$tiger1>getName()}.";
+		$tiger1 = new Tiger();
+		$tiger1->firstName = "Juanita ";
+		$tiger1->lastName = "Lopez";
+		print "The cats name is {$tiger1->getName()}. ";
 
 	class Elephant {
-		public $firstName = "";
-		public $lastName = "";
-		public $gender = "male";
-		public $price = "$100,000,000";
+		public $firstName = "";
+		public $lastName = "";
+		public $gender = "male";
+		public $price = "$100,000,000";
 
 		function getName() {
 		return "{$this->firstName}" .
-		"{$this->lastName}";
+		"{$this->lastName}";
 	}
 }
-		$elephant1 = new Elephant();
-		$elephant1->firstName = "Junior";
-		$elephant1->lastName = "Rodriguez";
-		print "The cat’s name is {$elephant1>getName()}.";
+		$elephant1 = new Elephant();
+		$elephant1->firstName = "Junior ";
+		$elephant1->lastName = "Rodriguez";
+		print "The cats name is {$elephant1->getName()}.";
 
 
-	class Cat {
-		public $firstName;
-		public $lastName;
-		public $breed;
+	class Cat2 {
+		public $firstName;
+		public $lastName;
+		public $breed;
 			
 			function __construct($title, $firstName, $lastName, $breed) {
-			$this->Juan = $firstName;
-			$this->Carlos = $lastName;
-			$this->siamese = $breed;
+			$this->Juan = $firstName;
+			$this->Carlos = $lastName;
+			$this->siamese = $breed;
 			}
 
 			function getName() {
-			return "{$this­>firstName}" .
-			"{$this­>lastName}";
+			return "{$this->firstName}" .
+			"{$this->lastName}";
 			}
 }
 
 
-	class Tiger {
-		public $firstName;
-		public $lastName;
-		public $breed;
+	class Tiger2 {
+		public $firstName;
+		public $lastName;
+		public $breed;
 			
 			function __construct($title, $firstName, $lastName, $breed) {
-			$this->Juanita = $firstName;
-			$this->Rodriguez = $lastName;
-			$this->siberian = $breed;
+			$this->Juanita = $firstName;
+			$this->Rodriguez = $lastName;
+			$this->siberian = $breed;
 			}
 
 			function getName() {
-			return "{$this­>firstName}" .
-			"{$this­>lastName}";
+			return "{$this->firstName}" .
+			"{$this->lastName}";
 			}
 }
 
-?>
-<?php
-	if (is_integer('1'))echo "It is an integer ;)";
-	else
-	echo 'It is not an integer';
-	var_dump(is_integer('2'));
-	var_dump(is_integer('3'));
-	var_dump(is_integer('4'));
-	var_dump(is_integer('5'));
-?>
 
-<?php
-	if (is_integer('1'))echo "It is an integer ;)";
-	else
-	echo 'It is not an integer';
-	var_dump(is_integer('2'));
-	var_dump(is_integer('3'));
-	var_dump(is_integer('4'));
-	var_dump(is_integer('5'));
-?>
+// 
+// 	if (is_integer('1'))echo "It is an integer )";
+// 	else
+// 	echo 'It is not an integer';
+// 	var_dump(is_integer('2'));
+// 	var_dump(is_integer('3'));
+// 	var_dump(is_integer('4'));
+// 	var_dump(is_integer('5'));
+// 
 
-<?php
-	if (is_integer('1'))echo "It is an integer ;)";
-	else
-	echo 'It is not an integer';
-	var_dump(is_integer('2'));
-	var_dump(is_integer('3'));
-	var_dump(is_integer('4'));
-	var_dump(is_integer('5'));
-?>
+// 
+// 	if (is_integer('1'))echo "It is an integer )";
+// 	else
+// 	echo 'It is not an integer';
+// 	var_dump(is_integer('2'));
+// 	var_dump(is_integer('3'));
+// 	var_dump(is_integer('4'));
+// 	var_dump(is_integer('5'));
+// 
+// 	if (is_integer('1'))echo "It is an integer )";
+// 	else
+// 	echo 'It is not an integer';
+// 	var_dump(is_integer('2'));
+// 	var_dump(is_integer('3'));
+// 	var_dump(is_integer('4'));
+// 	var_dump(is_integer('5'));
+// ?>
 
--- lecture4.php
+++ lecture4.php
@@ -1,59 +1,55 @@
 <?php
 class Animals {
 	public $firstName;
-	public $lastName;
-	public $scientificName;
-	public $gender;
-	public $weight;
-	public $bark;
-	public $meow;
+	public $lastName;
+	public $scientificName;
+	public $gender;
+	public $weight;
 
-	function __construct($scientificName, $firstName, $lastName, $gender, $weight, $bark, $meow) {
+	function __construct($scientificName, $firstName, $lastName, $gender, $weight) {
 		$this->scientificName = $scientificName;
 		$this->firstName = $firstName;
 		$this->lastName = $lastName;
-		$this->gender = $gender;
-		$this->weight = $weight;
-		$this->bark = $bark;
-		$this->meow = $meow;
+		$this->gender = $gender;
+		$this->weight = $weight;
 	}
 
 	function getName() {
-		return "This is my " . $this->firstName .
-		" and last " . $this->lastName;
-		"and this is my scientificName".$this->$scientificName;
+		return "my " . $this->firstName .
+		" and " . $this->lastName;
+		"and this is my scientificName".$this->$scientificName;
 	}
-
+}
 
 class Cat extends Animals {
-	function __construct($scientificName, $firstName, $lastName, $gender, $weight, $meow){
-			parent::__construct($scientificName, $firstName, $lastName, $gender, $weight, $meow);
-		$this->meow = $meow;
+	function __construct($scientificName, $firstName, $lastName, $gender, $weight){
+			parent::__construct($scientificName, $firstName, $lastName, $gender, $weight);
+		$this->weight = $weight;
 	}
 	function greet() {
-		return $this->meow;
+		return $this->weight;
 	}
 }
 
-class Dog extends Animal {
-	function __construct($scientificName, $firstName, $lastName, $gender, $weight, $bark){
-			parent::__construct($scientificName, $firstName, $lastName, $gender, $weight, $bark);
-		$this->bark = $bark;
+class Dog extends Animals {
+	function __construct($scientificName, $firstName, $lastName, $gender, $weight){
+			parent::__construct($scientificName, $firstName, $lastName, $gender, $weight);
+		$this->weight = $weight;
 	}
 	function hello() {
-		return $this->bark;
+		return $this->weight;
 	}
 }
 
 
-$cat = new Cat("Cat", "Mellow", "Yellow", "male", "15");
-print "Animal 1 is a " . $cat->getName();
+$cat = new Cat("Cat", "Mellow", "Yellow. ", "male", "15");
+print "Animal 1 is " . $cat->getName();
 ?>
 
 <?php
 class School {
 	public $name;
-	public $district;
+	public $district;
 	public $bathrooms;
 	public $classrooms;
 	public $teachers;
@@ -62,7 +58,7 @@ class School {
 
 	function __construct($name, $district, $bathrooms, $classrooms, $teachers, $students, $parents) {
 		$this->name = $name;
-		$this->district = $district;
+		$this->district = $district;
 		$this->bathrooms = $bathrooms;
 		$this->classrooms = $classrooms;
 		$this->teachers = $teachers;
@@ -72,14 +68,14 @@ class School {
 
 	function getName() {
 		return "I am " . $this->name .
-		" and I am district " . $this->lastName;
-		"and I have ".$this->$classrooms;
+		" and I am district " . $this->lastName;
+		"and I have ".$this->$classrooms;
 	}
-
+}
 
 class Teachers extends School {
 	function __construct($name, $district, $bathrooms, $classrooms, $teachers, $students, $parents){
-			parent::__construct($name, $district, $bathrooms, $classrooms, $teachers, $students, $parents);
+			parent::__construct($name, $district, $bathrooms, $classrooms, $teachers, $students, $parent);
 		$this->homework = $homework;
 	}
 	function greet() {
@@ -89,56 +85,54 @@ class Teachers extends School {
 
 class Parents extends School {
 	function __construct($name, $district, $bathrooms, $classrooms, $teachers, $students, $parents){
-			parent::__construct($name, $district, $bathrooms, $classrooms, $teachers, $students, $parents);
+			parent::__construct($name, $district, $bathrooms, $classrooms, $teachers, $students, $parent);
 		$this->feedback = $feedback;
 	}
 	function hello() {
-		return $this->feedback;
+		return $this->feedback;
 	}
+
 }
 
 
-$parents = new Parent("Parent", "Worried", "Mexican", "male", "45");
-print "My parent's name is " . $parent->getName();
+// $parent = new Parent("Father", "Worried", "Mexican", "male", "45");
+// print "My parent's name is " . $parent->getName();
 ?>
 
 <?php
 class Animal {
 	public $firstName;
-	public $lastName;
-	public $scientificName;
-	public $gender;
-	public $weight;
+	public $lastName;
+	public $scientificName;
+	public $gender;
+	public $weight;
 
-	public $meow;
-
-	function __construct($scientificName, $firstName, $lastName, $gender, $weight, $bark, $meow) {
+	function __construct($scientificName, $firstName, $lastName, $gender, $weight) {
 		$this->scientificName = $scientificName;
 		$this->firstName = $firstName;
 		$this->lastName = $lastName;
-		$this->gender = $gender;
-		$this->weight = $weight;
-		$this->bark = $bark;
-		$this->meow = $meow;
+		$this->gender = $gender;
+		$this->weight = $weight;
 	}
 
 	function getName() {
-		return "This is my " . $this->firstName .
-		" and last " . $this->lastName;
-		"and this is my scientificName".$this->$scientificName;
-	}
-
-
-class Tiger extends Animal {
-	function __construct($scientificName, $firstName, $lastName, $gender, $weight, $meow){
-			parent::__construct($scientificName, $firstName, $lastName, $gender, $weight, $meow);
-		$this->meow = $meow;
-	}
-	function greet() {
-		return $this->meow;
+		return " a " . $this->firstName .
+		" and " . $this->lastName;
+		"and this is my scientificName".$this->$scientificName;
 	}
 }
 
-$tiger = new Tiger("Tiger", "Hyper", "Striped", "male", "5");
-print "My animal is a " . $tiger->getName();
+
+class Tiger extends Animal {
+	function __construct($scientificName, $firstName, $lastName, $gender, $weight){
+			parent::__construct($scientificName, $firstName, $lastName, $gender, $weight);
+		$this->weight = $weight;
+	}
+	function greet() {
+		return $this->weight;
+	}
+}
+
+$tiger = new Tiger("Hyper", "Tiger", "Striped.", "male", "5");
+print "My animal is " . $tiger->getName();
 ?>
